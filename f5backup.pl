a7a7dbb4be9d7b8530841eaafcd3eeafce4352c8
Eric Flores <junkmaileric@gmail.com>
2013-10-27 14:54:42 -0500

    Version 2.4.1 -  Revamped shell detection  Fixed device dir creation bug  IP is updated in DB if a device IP changes in the device file  Other fixes

--- f5backup.pl
+++ f5backup.pl
@@ -20,15 +20,14 @@
 #####################################################################################
 
 # F5 Config backup 2.4 - 
-#	Added SQLite DB for back records
-#	Changed config change detection from hash to CID time
+#	Added SQLite DB for device records
 #	Added password based login and removed SSH key login
 #	Added TMSH detection to allow non root login
 #	Other misc fixes
-#
-# To do -
-# 	figure out a better way to detect TMSH
-# 	need new devices to update IP
+# Version 2.4.1 -
+#	Revamped shell detection
+#	Fixed device dir creation bug
+#	IP is updated in DB if a device IP changes in the device file
 #
 #
 
@@ -81,17 +80,17 @@ sub hms_time {
 };
 
 ############################################################################
-# ParseDeviceList - Parse name & IP from device list and output hash format
+# ParseDeviceList - Parse name & IP from device list and output list format
 # my %DEVICE_HASH = ParseDeviceList <DEVICE_LIST>;
 ############################################################################
 sub ParseDeviceList {
-	my @DEVICES= @_;
 	# Exclude lines that are comments and blank
-	@DEVICES = grep (!(/#/|/^\n/),@DEVICES);
+	my @DEVICES = grep (!(/#/|/^\n/),@_);
 	chomp(@DEVICES);
 	my @DEVICE_ARRAY;
 	foreach (@DEVICES) {
 		if ($_ =~ "=") {
+			# Parse NAME=IP format
 			my ($name,$ip) = split("=",$_);
 			push (@DEVICE_ARRAY,$name);
 			push (@DEVICE_ARRAY,$ip);
@@ -107,10 +106,12 @@ sub ParseDeviceList {
 # OrphansDBDelete - Remove orphaned devices from DB
 ############################################################################
 sub OrphansDBDelete {
-	# Check DB for orphaned devices 
+	# $START var is to keep sub from running sql query before start of main
 	if ($START) {
 		my @names_temp1 = @{$dbh->selectcol_arrayref("SELECT NAME FROM DEVICES")};
 		my @devices_temp1 = (keys %DEVICE_HASH);
+		# Loop through DB devices list, match against file device list
+		# Remove any devices from DB that are not in file
 		foreach (@names_temp1) {
 			if ( "@devices_temp1" !~ "$_") {
 				print LOG "Device $_ no longer in device list. Removing from DB at ",hms_time,".\n";
@@ -129,22 +130,56 @@ sub NewDeviceDB {
 	if ($START) {
 		foreach (keys %DEVICE_HASH) {
 			if (! join '',$dbh->selectrow_array("SELECT count('NAME') FROM DEVICES WHERE NAME = '$_'")) {
+				# If device is not in DB then add it
 				my $time = time;
 				print LOG "Device $_ is not in database. Adding to DB at ",hms_time,".\n";
 				unless ( $dbh->do("INSERT INTO DEVICES ('NAME','IP','CID_TIME','DATE_ADDED') 
 									VALUES ('$_','$DEVICE_HASH{$_}','0',$time)") ) {
 					print LOG "Error at ",hms_time,": Can't INSERT $_ to DB\n" ;
 				};
+			} else {
+				# If IP has changed in file then update DB
+				my $IP = join '',$dbh->selectrow_array("SELECT IP FROM DEVICES WHERE NAME = '$_'");
+				if ($IP ne $DEVICE_HASH{$_} ) {
+					print LOG "IP has changed for $_. Old IP is $IP. Updating to new IP of $DEVICE_HASH{$_}\n";
+					unless ( $dbh->do("UPDATE DEVICES SET IP = '$DEVICE_HASH{$_}' WHERE NAME = '$_'") ) {
+						print LOG "Error at ",hms_time,": Can't INSERT $_ to DB\n" ;
+					};
+				};
 			};
 		};
 	};
 };
 ############################################################################
+# DetectShell - find out which shell the device is in
+# my $shell = DetectShell $_,$ssh 
+############################################################################
+sub DetectShell {
+	my ($DEVICE,$SSH) = (@_);
+	my ($output,$discard) = $SSH->capture2("echo hello");
+	chomp $output;
+	# if output is hello the shell is bash
+	if ($output eq "hello")  {
+		print LOG "Shell for $DEVICE is bash.\n";
+		return "bash";
+	};
+	($output,$discard) = $SSH->capture2("show sys version");
+	chomp $output;
+	# If output contains Sys::Version then shell is TMSH
+	if ($output =~ "Sys::Version")  {
+		print LOG "Shell for $DEVICE is tmsh.\n";
+		return "tmsh";
+	};
+};
+
+############################################################################
 # CreateDeviceDIR - Make a new device folder if does not exist
 # CreateDeviceDIR [DEVICE];
 ############################################################################
 sub CreateDeviceDIR {
-	my $DEVICE = @_;
+	my ($DEVICE) = @_;
+	print LOG "device - $DEVICE, dev - $DEVICE\n";
+	# If you cant open the dir then it does not exist
 	unless (opendir(DIRECTORY,"$ARCHIVE_DIR/$DEVICE")) {
 		print LOG "Device directory $ARCHIVE_DIR/$DEVICE does not exist. Creating folder $DEVICE at ",hms_time,".\n";
 		my $NEW_DIR = "$ARCHIVE_DIR/$DEVICE";
@@ -153,27 +188,27 @@ sub CreateDeviceDIR {
 			$ERROR++;
 			next;
 		};
+	} else {
+		closedir DIRECTORY;
 	};
 };
 
 ############################################################################
 # GetCIDtime & ParseDBkey - Get the CID time from device
-# my $new_cid_time = GetCIDtime([device],[ssh_handle]);
+# my $new_cid_time = GetCIDtime [device],[ssh_handle],[shell];
 ############################################################################
 sub ParseDBkey {
 	my $text = join '',@_;
+	# get value between quotes
 	$text = join '',($text =~ /"(.+?)"/);
 	return $text;
 };
-
 sub GetCIDtime {
-	my ($DEVICE,$SSH) = (@_);
-	my ($output,$errput) = $SSH->capture2("echo tmsh");
-	chomp ($output,$errput);
+	my ($DEVICE,$SSH,$SHELL) = (@_);
 	my $DB_KEY;
-	if ($output eq "tmsh")  {
+	if ($SHELL eq "bash")  {
 		$DB_KEY = ParseDBkey $SSH->capture("tmsh list sys db configsync.localconfigtime");
-	} else {
+	} elsif ($SHELL eq "tmsh") {
 		$DB_KEY = ParseDBkey $SSH->capture("list sys db configsync.localconfigtime");
 	};
 	print LOG "CID time for $DEVICE is - $DB_KEY.\n";
@@ -182,17 +217,15 @@ sub GetCIDtime {
 
 ############################################################################
 # CreateUCS - Creates UCS file on device
-# CreateUCS([device],[ssh_handle]);
+# CreateUCS [device],[ssh_handle],[shell];
 ############################################################################
 sub CreateUCS {
-	my ($DEVICE,$SSH) = (@_);
+	my ($DEVICE,$SSH,$SHELL) = (@_);
 	print LOG "CID times do not match for $DEVICE at ",hms_time,". Downloading backup file.\n";
-	my ($tmsh,$discard) = $SSH->capture2("echo tmsh");
-	chomp $tmsh;
 	my ($output,$errput);
-	if ($tmsh eq "tmsh")  {
+	if ($SHELL eq "bash")  {
 		($output,$errput) = $SSH->capture2("tmsh save sys ucs /shared/tmp/backup.ucs");
-	} else {
+	} elsif ($SHELL eq "tmsh") {
 		($output,$errput) = $SSH->capture2("save sys ucs /shared/tmp/backup.ucs");
 	};
 	chomp $errput;
@@ -221,9 +254,11 @@ sub GetUCS {
 ############################################################################
 sub CleanArchive {
 	my @DEVICES = @_;
+	# Go into each device directory
 	foreach (@DEVICES) {
 		my $DEVICE = $_;
 		if (opendir(DIRECTORY,"$ARCHIVE_DIR/$DEVICE")) { 
+			# Delete files in array with index higher than archive size
 			my @DIRECTORY = readdir(DIRECTORY);
 			@DIRECTORY = reverse sort grep(/backup.ucs/,@DIRECTORY); 
 			foreach (@DIRECTORY[$UCS_ARCHIVE_SIZE..($#DIRECTORY)]) {
@@ -244,6 +279,7 @@ sub CleanArchive {
 ############################################################################
 sub CleanLogs {
 	if (opendir(DIRECTORY,"$DIR/log/")) {
+		# Delete files in array with index higher than archive size
 		my @DIRECTORY = readdir(DIRECTORY);
 		@DIRECTORY = reverse sort grep(/backup.log/,@DIRECTORY);
 		foreach (@DIRECTORY[$LOG_ARCHIVE_SIZE..($#DIRECTORY)]) {
@@ -268,19 +304,22 @@ open LOG,"+>","$DIR/log/$DATE-backup.log";
 print LOG "Starting configuration backup on $DATE at ",hms_time,".\n";
 
 # Open device list, create device list hash
+print LOG "Opening device list file /$DIR/$DEVICE_LIST at ",hms_time,".\n";
 open DEVICE_LIST,"<","$DIR/$DEVICE_LIST" or die "Cannot open device list - $!.\n";
 %DEVICE_HASH = ParseDeviceList <DEVICE_LIST>;
 close DEVICE_LIST;
 
 # Get password from file
+print LOG "Opening password file $PASS_FILE at ",hms_time,".\n";
 open PSWD,"<","$PASS_FILE" or die "Error at ",hms_time,": Can't open password file - $!\n" ;
 my $PASSWORD = <PSWD>;
 chomp $PASSWORD;
 close PSWD;
 
 # Connect to DB
+print LOG "Opening DB file $DIR/$DB_FILE at ",hms_time,".\n";
 $dbh = DBI->connect(          
-    "dbi:SQLite:dbname=$DB_FILE", 
+    "dbi:SQLite:dbname=$DIR/$DB_FILE", 
     "",
     "",
     { RaiseError => 1}
@@ -291,6 +330,7 @@ OrphansDBDelete;
 
 # Add new devices to DB
 NewDeviceDB;
+undef %DEVICE_HASH;
 
 # Get device names from DB
 my @DEVICES_NAMES = @{$dbh->selectcol_arrayref("SELECT NAME FROM DEVICES")};
@@ -301,7 +341,7 @@ foreach (@DEVICES_NAMES) {
 
 	# Create device folder is it does not exist
 	CreateDeviceDIR $_;
-
+	
 	# Get IP from DB or set to hostname if NULL
 	my $IP = join '',$dbh->selectrow_array("SELECT IP FROM DEVICES WHERE NAME = '$_'");
 	$IP = $_ if ($IP eq 'NULL');
@@ -319,10 +359,13 @@ foreach (@DEVICES_NAMES) {
 		next;
 	};
 
+	# Detect device shell
+	my $shell = DetectShell $_,$ssh;
+	
 	# get cid time from device and write to VAR
-	my $new_cid_time = GetCIDtime($_,$ssh);
+	my $new_cid_time = GetCIDtime $_,$ssh,$shell;
 
-	# Check for new cid time and break if it does not exist
+	# Check for new cid time or next if it does not exist
 	if (! defined $new_cid_time) {
 		print LOG "Get CID time failed for $_ at ",hms_time,". Skipping to next device.\n";
 		$ERROR++;
@@ -339,7 +382,7 @@ foreach (@DEVICES_NAMES) {
 	} else {
 		# Make device create UCS file, Download UCS file, Disconnect SSH session, Write new cid time to DB
 		my $time = time;
-		CreateUCS($_,$ssh);
+		CreateUCS $_,$ssh,$shell;
 		GetUCS($_,$ssh);
 		undef $ssh;
 		unless ( $dbh->do("UPDATE DEVICES SET CID_TIME = '$new_cid_time', 
@@ -353,7 +396,7 @@ foreach (@DEVICES_NAMES) {
 print LOG "\nDeleting old files:\n";
 
 # Keep only the number of UCS files specified by UCS_ARCHIVE_SIZE and write deletion to log
-CleanArchive keys %DEVICE_HASH;
+CleanArchive @DEVICES_NAMES;
 
 # Keep only the number of log files specified by LOG_ARCHIVE_SIZE and write deletion to log
 CleanLogs;
